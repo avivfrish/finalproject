ed52b50f38461887a8b98b05be1b48a454f6bd9e
avivf <avivf@campus.technion.ac.il>
2018-11-15 19:13:44 +0200

    fix angular js

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
     <!--<script src="js/bootstrap.min.js"></script>-->
-	<script src="js/project.js"></script>
+	<script src="js/project.js" ></script>
 		
 </head>
 <body ng-controller='ng-cases' ng-init="init_case();">
