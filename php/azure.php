47fea33cf907e27016dfdabed600d9d60b439976
avivf <avivf@campus.technion.ac.il>
2018-11-15 10:16:20 +0200

    fix js azure func try6

--- php/azure.php
+++ php/azure.php
@@ -1,16 +1,9 @@
 // PHP Data Objects(PDO) Sample Code:
 <?php
-try {
-    $conn = new PDO("sqlsrv:server = tcp:avifinalproject.database.windows.net,1433; Database = finalProject", "finalproject", "1qaZ2wsX");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "finalproject@avifinalproject", "pwd" => "{your_password_here}", "Database" => "finalProject", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "finalproject@avifinalproject", "pwd" => "1qaZ2wsX", "Database" => "finalProject", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:avifinalproject.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
@@ -19,15 +12,18 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 $sql= /** @lang text */
     "select name from companies";
 $getResults= sqlsrv_query($conn, $sql);
-echo ("Reading data from table" . PHP_EOL);
+//echo ("Reading data from table" . PHP_EOL);
 if ($getResults == FALSE)
-    echo (sqlsrv_errors());
-$arr=array();
+    return (sqlsrv_errors());
+$array = array();
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-   $arr['name']=$row['name'];
+    $array[] = array(
+            'name'=>$row['name']
+    );
 }
-print json_encode($arr);
 sqlsrv_free_stmt($getResults);
+echo json_encode($array);
+
 
 ?>
 ?>
