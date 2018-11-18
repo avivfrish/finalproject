abbc8c3be402890578f293f9376542282512bd92
CORAL SCHARF <coralscharf@gmail.com>
2018-11-18 10:42:01 +0200

    update search coral

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <html lang="en" ng-app="template" >
 <head>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-	<!--<link rel="stylesheet" href="css/project.css">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <!--<link rel="stylesheet" href="css/project.css">-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
     <!--<script src="js/bootstrap.min.js"></script>-->
-	<script src="js/project.js" ></script>
+    <script src="js/project.js" ></script>
 		
 </head>
 <body ng-controller='ng-cases' ng-init="init_case();">
