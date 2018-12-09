a72408ec27da2dfa70b3cc4692bc0c16613094f6
RoniRec <ronit.rec@gmail.com>
2018-12-09 17:39:23 +0200

    test

---
+++ php/insertNewComp.php
@@ -0,0 +1,28 @@
+<?php
+$connectionInfo = array("UID" => "finalproject@avifinalproject", "pwd" => "1qaZ2wsX", "Database" => "finalProject", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:avifinalproject.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+$sql =/** @lang text */
+    "INSERT INTO companies(ID,name,street,country,state) Values('".$_GET['companyID']."','".$_GET['companyName']."','".$_GET['street']."','".$_GET['country']."','".$_GET['state']."')";
+    $getResults= sqlsrv_query($conn, $sql);
+
+/*if ($getResults == FALSE) {
+    echo ("no1");
+
+    //return (sqlsrv_errors());
+}
+else {
+    echo ("yes");
+}*/
+
+//$array = array();
+//while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+//    $array[] = array(
+//        'name'=>$row['name']
+//    );
+//}
+sqlsrv_free_stmt($getResults);
+//echo json_encode($array);
+
+//
