30d8c4778dfaa767872568860766166f2bda35a2
CORAL SCHARF <coralscharf@gmail.com>
2018-12-18 17:07:43 +0200

    insert stock graph function to stock tab

--- js/project.js
+++ js/project.js
@@ -542,6 +542,27 @@ app.controller('ng-cases', function ($scope, $http, $interval, fileUpload) {
         //document.getElementById("selectedResult").innerText = name;
 
     }
+    
+    $scope.createNewStockGraph = function (name) {
+	    console.log("CREATE STOCK GRAPH");
+        document.getElementById("fillHrefForStock").innerHTML = '<a href="https://www.tradingview.com/symbols/NYSE-ALLY/" rel="noopener" target="_blank"><span class="blue-text">AAPL chart</span></a>';
+        new TradingView.widget(
+            {
+                "width" : "500px",
+                "height": "450px",
+                "interval": "D",
+                "timezone": "Etc/UTC",
+                "theme": "Light",
+                "style": "1",
+                "locale": "en",
+                "toolbar_bg": "#f1f3f6",
+                "enable_publishing": false,
+                "allow_symbol_change": true,
+                "container_id": "tradingview_a5966"
+            }
+        );
+
+    }
 
 });	 //app.controller
 
